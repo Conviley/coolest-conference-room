a68eafb22493d447f24cab2305754eaacfa0324a
Tjelvar Guo <tjegu689@student.liu.se>
2018-12-09 19:59:38 +0100

    add call shia demo

--- Coolest Conference Room/Assets/Scripts/AppManager.cs
+++ Coolest Conference Room/Assets/Scripts/AppManager.cs
@@ -1,27 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AppManager : MonoBehaviour {
 
     public GameObject initialUi;
-    public GameObject translationUi;
+
     private GameObject imageTarget;
+    private AudioSource audioSource;
+    public List<AudioClip> audioClips;
+    public List<Sprite> phoneIcons;
+    public Button phoneButton;
+    public GameObject callShieText;
+
+    private bool instantiateShia = false;
+    private bool calling = false;
 
     private void Start () {
-        translationUi.SetActive(false);
         imageTarget = GameObject.Find("ImageTarget");
+        audioSource = GetComponent<AudioSource>();
 	}
 
     private void Update() {
-
+        if (instantiateShia && !audioSource.isPlaying) {
+            Instantiate(Resources.Load("VideoPlane"), imageTarget.transform);
+            instantiateShia = false;
+            callShieText.active = false;    
+        }
     }
 
-    public void ShowTranslationUI() {
+    public void HandlePhoneButtonClick() {
+        if (!calling) {
+            CallShia();
+            calling = true;
+        } else {
+            HangUp();
+            calling = false;
+        }
+    }
 
+    private void HangUp() {
+        audioSource.Stop();
+        audioSource.PlayOneShot(audioSource.clip);
+        Destroy(GameObject.Find("VideoPlane"));
+        phoneButton.GetComponent<Image>().sprite = phoneIcons[1];
+        callShieText.active = false;
+        instantiateShia = false;
     }
 
     public void instantiateObject() {
         Instantiate(Resources.Load("Cube"), imageTarget.transform);
+    }
+
+    private void CallShia() {
+        audioSource.PlayOneShot(audioClips[0]);
+        instantiateShia = true;
+        callShieText.active = true;
+        phoneButton.GetComponent<Image>().sprite = phoneIcons[0];
     }
 }
